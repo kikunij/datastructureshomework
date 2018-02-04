8a910fc9484bd820b2d559d5534aa5abc42ff8a6
Joachim Kikuni <joachim1@graceland.edu>
2018-02-04 13:41:48 -0600

    website update

--- JoachimKikuni.Lab02/JoachimKikuni_Stimulation.py
+++ JoachimKikuni.Lab02/JoachimKikuni_Stimulation.py
@@ -1,11 +1,28 @@
 import random
 
+names = ["joey", "bobby", "susan", "Loretta", "grant",\
+         "jenny"]
+waitingRoom = []
+triageRoom = []
+examRoom = []
+examRoomSize = 6
+doctors = 6
+
+
+
 class Physician:
     def __init__(self, name):
 
         self.name = name
         
     def examination():
+
+    def callNurse():
+        "move patient from waiting room to triage room"
+    triageRoom.append(waitingRoom.pop(0))
+    sort(triageRoom, key=patient.triageNumber)
+    
+         
 
 class nurse: 
 
@@ -19,7 +36,24 @@ class nurse:
 
 class Patient:
     def __init__(self):
-        self.name = name
+        self.triageNumber = random.randint(100)
+        self.name = names[random.randint(len(names))]\
+                     +names[random.randint(len(names))]
+        self.arrivalTime = time
+        self.treatmentTime = random.randint(15, 20)
+        
+        
+        #currentLocation
+        #triageTime
+        #name
+        #arrivalTime
+        #treatmentTime(15-20 minutes)
+
+    def exit(self):
+        #remove patient from simulation
+        pass
+    
+        
 
 class ExamRoom:
     def __init__(self, name, physician):
