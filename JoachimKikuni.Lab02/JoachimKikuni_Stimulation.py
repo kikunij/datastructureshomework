aa1f01c1a0bd3b1b661f316e09ccdd07123d27ee
Joachim Kikuni <joachim1@graceland.edu>
2018-02-05 01:02:43 -0600

    website update

--- JoachimKikuni.Lab02/JoachimKikuni_Stimulation.py
+++ JoachimKikuni.Lab02/JoachimKikuni_Stimulation.py
@@ -1,94 +1,75 @@
 import random
-
+import time 
 names = ["joey", "bobby", "susan", "Loretta", "grant",\
-         "jenny"]
+         "jenny", "Pedelson", "Amadou", "Ajata", "Marie",\
+         "Albert", "Jonathan", "Dylan"]
 waitingRoom = []
 triageRoom = []
-examRoom = []
 examRoomSize = 6
 doctors = 6
+triageRoomSize = 10
+examRoom = []
+patientsTreated = []
+triageNumber = 10
 
 
-
-class Physician:
-    def __init__(self, name):
-
-        self.name = name
-        
-    def examination():
-
-    def callNurse():
-        "move patient from waiting room to triage room"
+def callNurse():
+    "move patient from waiting room to triage room"
     triageRoom.append(waitingRoom.pop(0))
-    sort(triageRoom, key=patient.triageNumber)
-    
-         
-
-class nurse: 
-
-    def __init__(self, name):
-        self.name = name
-
-    def getPatient():
-
-    def sendPatient():
-
+    triageRoom.sort(key =lambda Patient: Patient.triageNumber)
 
 class Patient:
-    def __init__(self):
-        self.triageNumber = random.randint(100)
-        self.name = names[random.randint(len(names))]\
-                     +names[random.randint(len(names))]
+    def __init__(self, time):
+        self.triageNumber = random.randint(1, 100)
+        self.name = names[random.randint(0, len(names)-1)]\
+                     + " " +names[random.randint(0, len(names)-1)]
         self.arrivalTime = time
         self.treatmentTime = random.randint(15, 20)
-        
-        
-        #currentLocation
-        #triageTime
-        #name
-        #arrivalTime
-        #treatmentTime(15-20 minutes)
+        self.timeEnteredRoom = 0
+              
+def simulation():
+    time.clock()
+    patientTreated = 0
+    minute = 1
+    while minute != 11:
+        print("minute", minute)
 
-    def exit(self):
-        #remove patient from simulation
-        pass
-    
-        
+        if minute % 2 ==0:
+            for i in range(6):
+                waitingRoom.append(Patient(minute+1))
+        if len(triageRoom) < triageRoomSize:
+            if len(waitingRoom) < triageRoomSize:
+                for i in range(len(triageRoom), len(waitingRoom)):
+                    callNurse()
+            elif len(waitingRoom) < triageRoomSize:
+                for i in range(len(triageRoom), triageRoomSize):
+                    callNurse()
+        if len(triageRoom) < triageRoomSize:                        
+            for i in range(6):
+                waitingRoom.append(Patient(minute))
+                
+            for i in range(len(waitingRoom)):
+                callNurse()
+            if len(examRoom) < examRoomSize:
+                for i in range (len(examRoom), examRoomSize):
+                    nextPatient = triageRoom.pop(0)
+                    examRoom.append(nextPatient)
+                    nextPatient.timeEnteredExamRoom = minute + 1
+                             
 
-class ExamRoom:
-    def __init__(self, name, physician):
-        
-        self.name = name
-        self.physician = physician
-        self.runTime = 0
-        self.Patient = None
-        
-    def addPatients(self, waitingRoom):
-        self.waitingList2.pop(0)
+        print("patients in exam room")
+        for p in examRoom:
+            print(p.name, "treatment time", p.treatmentTime, p.timeEnteredExamRoom)
+            
+        print("patients in Triage room")
+        for p in triageRoom:
+            print(p.name, "treatment time", p.triageNumber)
 
-    
-    def removePatients(self):
-    
-    def timeRemaining(self):
-        return self.runTime
-
-class WaitingRoom:
-    
-    def __innit__(self):
-        self.waitingList1 = []
-        self.waitingList2 = []
-        
-        
-    def addPatients(self, patient):
-        self.waitingList.append(patient)
-        
-    
-    def removePatients(self):
-        self.waitingList.pop(0)
-
-    def addPatientsTwo():
-        self.waitingList2.append(patient)
-        
-    def removePatientsTwo:
-        return self.waitingList2.pop(0)
-
+        print("patients in waiting room")
+        for p in waitingRoom:
+            print(p.name)
+        print("patients treated:", patientsTreated)
+        print("\n") 
+        time.sleep(1)
+        minute += 1
+simulation()
